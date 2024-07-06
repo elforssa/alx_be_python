51fd877935da15d119852a9178865af0a0a56e9f
Elforssae <elforssa@gmail.com>
2024-07-06 23:33:20 +0200

    add

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,22 @@
-length = 10
-width = 5
-area = length * width
-print(f"The area of the rectangle is: {area}")
+#length = 10
+#width = 5
+#area = length * width
+#print(f"The area of the rectangle is: {area}")
+
+day = input("Enter a day of the week (Monday-Sunday): ").lower()
+
+match day:
+    case "monday":
+        print("Ugh, Mondays...")
+    case "tuesday":
+        print("Just another workday...")
+    case "wednesday":
+        print("Hump day!")
+    case "thursday":
+        print("Almost there...")
+    case "friday":
+        print("TGIF!")
+    case "saturday" | "sunday":  # Match multiple values with pipe (|)
+        print("Weekend vibes!")
+    case _:
+        print("Invalid day entered.")
